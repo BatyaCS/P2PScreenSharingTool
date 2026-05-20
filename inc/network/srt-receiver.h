d7ebd4b523152675e46623511e404fb41975c31b
Aleksei <lehich1112228@gmail.com>
2026-05-20 10:52:01 +0300

    Added class SrtReceiver

---
+++ inc/network/srt-receiver.h
@@ -0,0 +1,47 @@
+#ifndef SRT_RECEIVER_H_
+#define SRT_RECEIVER_H_
+
+#include <string>
+#include <vector>
+#include <mutex>
+
+#include <network/srt-defs.h>
+#include <utils/non-copyable.h>
+
+class SrtReceiver : NonCopyable
+{
+public:
+    struct NetworkConfig
+    {
+        SrtEncryption   encryption = SrtEncryption::AES_128;
+        SrtLatePktDrop  packet_drop = SrtLatePktDrop::ENABLE;
+
+        std::string     stream_id;
+        std::string     stream_pwd;
+
+        std::string     ip;
+        uint            port;
+
+        static constexpr size_t buffer_size = 10 * 1024 * 1024;
+        static constexpr uint stream_latency_ms = 300;
+    };
+
+    SrtReceiver();
+    ~SrtReceiver() { close_connection(); }
+
+    bool is_connected() const;
+
+    bool open_connection(const NetworkConfig& config);
+    bool close_connection();
+
+    bool receive(std::vector<uint8_t>& out_data);
+
+private:
+    using SRTSOCKET = int32_t;
+
+    SRTSOCKET _socket;
+
+    std::mutex _receive_mutex;
+};
+
+#endif /* SRT_RECEIVER_H_ */
---
+++ src/network/srt-receiver.cpp
@@ -0,0 +1,114 @@
+#include <common.h>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <srt/srt.h>
+
+#include <network/srt-receiver.h>
+
+SrtReceiver::SrtReceiver()
+    : _socket(SRT_INVALID_SOCK)
+{}
+
+bool SrtReceiver::is_connected() const 
+{ 
+    return _socket != SRT_INVALID_SOCK; 
+}
+
+bool SrtReceiver::open_connection(const NetworkConfig& config) 
+{
+    close_connection();
+
+    _socket = srt_create_socket();
+    if (_socket == SRT_INVALID_SOCK)
+    {
+        LOG_ERROR("Failed to create SRT socket to open connection!\n");
+        return false;
+    }
+
+    srt_setsockopt(_socket, 0, SRTO_STREAMID, config.stream_id.c_str(), static_cast<int>(config.stream_id.length()));
+    
+    if (!config.stream_pwd.empty()) 
+    {
+        const int key_len = static_cast<int>(config.encryption);
+        const int pwd_len = static_cast<int>(config.stream_pwd.length());
+        
+        if (pwd_len > key_len)
+        {
+            LOG_ERROR("Incorrect password length is set to open connection!\n");
+            return false;
+        }
+        
+        srt_setsockopt(_socket, 0, SRTO_PBKEYLEN, &key_len, sizeof(key_len));
+        srt_setsockopt(_socket, 0, SRTO_PASSPHRASE, config.stream_pwd.c_str(), pwd_len);
+    }
+
+    const int buf_size = static_cast<int>(config.buffer_size);
+    srt_setsockopt(_socket, 0, SRTO_RCVBUF, &buf_size, sizeof(buf_size));
+
+    const int pkt_drop = static_cast<int>(config.packet_drop);
+    srt_setsockopt(_socket, 0, SRTO_TLPKTDROP, &pkt_drop, sizeof(pkt_drop));
+
+    const int latency_ms = static_cast<int>(config.stream_latency_ms);
+    srt_setsockopt(_socket, 0, SRTO_LATENCY, &latency_ms, sizeof(latency_ms));
+    
+    sockaddr_in sa{};
+        
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(config.port);
+    inet_pton(AF_INET, config.ip.c_str(), &sa.sin_addr);
+
+    if (SRT_ERROR == srt_connect(_socket, (struct sockaddr*)&sa, sizeof(sa))) 
+    {
+        LOG_ERROR("Failed to init SRT connect! Error: %s\n", srt_getlasterror_str());
+        
+        close_connection();
+        return false;
+    }
+
+    LOG("SRT Connection opened to %s:%u for receiving!\n", config.ip.c_str(), config.port);
+    return true;
+}
+
+bool SrtReceiver::close_connection()
+{
+    if (_socket == SRT_INVALID_SOCK)
+        return true;
+
+    if (SRT_ERROR == srt_close(_socket))
+    {
+        LOG_ERROR("Failed to close SRT connection on socket\n");
+        return false;
+    }
+
+    _socket = SRT_INVALID_SOCK;
+    return true;
+}
+
+bool SrtReceiver::receive(std::vector<uint8_t>& out_data) 
+{
+    static constexpr size_t MAX_RECEIVE_SIZE_BYTES = 8192;
+
+    std::lock_guard<std::mutex> lock(_receive_mutex);
+
+    if (SRT_INVALID_SOCK == _socket)
+    {
+        LOG_ERROR("Failed to receive SRT data, socket isn't connected!\n");
+        return false;
+    }
+
+    out_data.resize(MAX_RECEIVE_SIZE_BYTES);
+    const int bytes_received = srt_recvmsg(_socket,  
+                                           reinterpret_cast<char*>(out_data.data()), 
+                                           static_cast<int>(MAX_RECEIVE_SIZE_BYTES));
+    
+    if (SRT_ERROR == bytes_received) 
+    {
+        LOG_ERROR("Failed to receive SRT chunk! Error: %s\n", srt_getlasterror_str());
+        out_data.clear();
+        return false;
+    }
+
+    out_data.resize(static_cast<size_t>(bytes_received));    
+    return true;
+}
